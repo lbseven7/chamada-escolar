64543578538ff1f3e4e37813dc2788c64e1c2775
lbseven7 <leobarbosapinturas@gmail.com>
2023-06-01 10:28:18 -0300

    feat: chama componentes no App

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,9 @@
-import Loading from './components/Loading';
-// import Chamada from './components/Chamada';
-import Login from './pages/Login';
-// import Table from './components/Table';
-
 function App() {
   return (
     <div>
-      {/* <Chamada /> */}
-      <Loading />
-      <Login />
+      <h1>
+        Arlisson
+      </h1>
     </div>
   );
 }

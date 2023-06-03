35cbefda88fba7d63c8ac43a464aae8549c106d1
Arlisson N. Costa <arlisson314@gmail.com>
2023-06-03 07:16:38 -0300

    feat: Cria arquivo para centralizar exportação de imagens

---
+++ src/images/icons/index.jsx
@@ -0,0 +1,8 @@
+import lock from './lock.png';
+import eye from './eye.png';
+import eyeClosed from './eye-closed.png';
+import user from './user.png';
+
+export {
+  lock, eye, eyeClosed, user,
+};

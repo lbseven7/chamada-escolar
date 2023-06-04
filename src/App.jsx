f607ecdda63ffb652a7d66ac1b291b0140388c9c
Arlisson N. Costa <arlisson314@gmail.com>
2023-06-04 17:26:57 -0300

    feat: test

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Loading from './components/Loading';
+import SignUp from './pages/SignUp';
 
 function App() {
   return (
     <div>
       <Loading />
+      <SignUp />
     </div>
   );
 }

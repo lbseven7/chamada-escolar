e4aba088037eab87c95ce47fe7f3043ddc6a8bdb
lbseven7 <leobarbosapinturas@gmail.com>
2023-06-04 20:04:01 -0300

    feat: cria o componente de Login

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-import Loading from './components/Loading';
-import SignUp from './pages/SignUp';
+// import Loading from './components/Loading';
+// import SignUp from './pages/SignUp';
+
+import Login from './pages/Login';
 
 function App() {
   return (
     <div>
-      <Loading />
-      <SignUp />
+      {/* <Loading /> */}
+      {/* <SignUp /> */}
+      <Login />
     </div>
   );
 }

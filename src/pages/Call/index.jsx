8ce6e361ac4c264c7fe76a933c63d5991ff45b4d
lbseven7 <leobarbosapinturas@gmail.com>
2024-04-02 21:50:40 -0300

    feat: chama componente Header nas páginas Call e Edit

--- src/pages/Call/index.jsx
+++ src/pages/Call/index.jsx
@@ -1,17 +1,21 @@
 import { Link } from 'react-router-dom';
+import Header from '../../components/Header';
 
 function Call() {
   return (
-    <div className="flex justify-center mt-72">
-      <h1>AQUI FAZER CHAMADA</h1>
-      <div>
-        <Link to="/home">
-          <button className="bg-btn-color p-5 mt-36" type="submit">
-            voltar para home
-          </button>
-        </Link>
+    <>
+      <Header />
+      <div className="flex justify-center mt-72">
+        <h1>AQUI FAZER CHAMADA</h1>
+        <div>
+          <Link to="/home">
+            <button className="bg-btn-color p-5 mt-36" type="submit">
+              voltar para home
+            </button>
+          </Link>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
--- src/pages/EditCall/index.jsx
+++ src/pages/EditCall/index.jsx
@@ -1,18 +1,22 @@
 import { Link } from 'react-router-dom';
+import Header from '../../components/Header';
 
 function Edit() {
   return (
-    <div className="flex justify-center mt-72 text-center">
-      <h1>AQUI EDITAR CHAMADA</h1>
+    <>
+      <Header />
+      <div className="flex justify-center mt-72 text-center">
+        <h1>AQUI EDITAR CHAMADA</h1>
 
-      <div>
-        <Link to="/home">
-          <button className="bg-btn-color p-5 mt-36 mr-24" type="submit">
-            voltar para home
-          </button>
-        </Link>
+        <div>
+          <Link to="/home">
+            <button className="bg-btn-color p-5 mt-36 mr-24" type="submit">
+              voltar para home
+            </button>
+          </Link>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 

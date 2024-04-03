ce39f41892b1e02035f9936a2dbe8a2c63a52b8a
lbseven7 <leobarbosapinturas@gmail.com>
2024-04-02 21:13:09 -0300

    feat: chama componente Link na página de editar chamadas

--- src/pages/Edit/index.jsx
+++ src/pages/Edit/index.jsx
@@ -1,7 +1,17 @@
+import { Link } from 'react-router-dom';
+
 function Edit() {
   return (
     <div className="flex justify-center mt-72">
       <h1>AQUI EDITAR CHAMADA</h1>
+
+      <div>
+        <Link to="/home">
+          <button className="bg-btn-color p-5" type="submit">
+            voltar para home
+          </button>
+        </Link>
+      </div>
     </div>
   );
 }

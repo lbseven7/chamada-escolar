4abe278e5fcc58512c77744121afd418e14889ac
lbseven7 <leobarbosapinturas@gmail.com>
2024-04-02 21:12:21 -0300

    feat: chama componente LInk na page editar

--- src/pages/Call/index.jsx
+++ src/pages/Call/index.jsx
@@ -1,7 +1,16 @@
+import { Link } from 'react-router-dom';
+
 function Call() {
   return (
     <div className="flex justify-center mt-72">
       <h1>AQUI FAZER CHAMADA</h1>
+      <div>
+        <Link to="/home">
+          <button className="bg-btn-color p-5" type="submit">
+            voltar para home
+          </button>
+        </Link>
+      </div>
     </div>
   );
 }

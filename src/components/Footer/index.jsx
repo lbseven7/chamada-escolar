cb201b939244b1f6c4bc96827fcf60093e6f6704
lbseven7 <leobarbosapinturas@gmail.com>
2024-04-02 22:55:28 -0300

    style: adiciona estilos no footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,7 +1,12 @@
 function Footer() {
   return (
-    <div className="bg-secundary-bg h-[70px] w-full">
-      <h4>Arlisson Nascimento Costa.</h4>
+    <div className="bg-secundary-bg h-[70px] w-full flex items-center justify-center">
+      <h4
+        className="flex-wrap text-primary-bg text-center pt-1 text-sm"
+      >
+        @Chamada Escolar. Todos os direitos reservados 2024.
+        <a className="underline hover:black" href="https://github.com/lbseven7" target="_blank" rel="noreferrer">lbseven7</a>
+      </h4>
     </div>
   );
 }
